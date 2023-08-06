315c98171f135453727387a22eb498fbc4e20970
Dmitry <99917230+dnp34@users.noreply.github.com>
2023-08-06 18:14:05 +0300

    Create Main1.java

---
+++ Main1.java
@@ -0,0 +1,43 @@
+public class Main {
+
+    // Создание класса, который реализует интерфейс Runnable
+    static class MyRunnable implements Runnable {
+        private final String name;
+
+        public MyRunnable(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 5; i++) {
+                System.out.println("Thread " + name + " iteration: " + i);
+                try {
+                    // Приостанавливаем поток на 100 миллисекунд
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        // Создаем два потока, которые выполняются параллельно
+        Thread thread1 = new Thread(new MyRunnable("A"));
+        Thread thread2 = new Thread(new MyRunnable("B"));
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            // Дожидаемся завершения выполнения каждого потока
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All threads have finished execution");
+    }
+}
