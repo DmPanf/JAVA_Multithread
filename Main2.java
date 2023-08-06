0bb6e2033d838bf12cdde06fac8f93c40bbec3d7
Dmitry <99917230+dnp34@users.noreply.github.com>
2023-08-06 18:16:36 +0300

    Update Main2.java

--- Main2.java
+++ Main2.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main2 {
 
     static class SystemInfoRunnable implements Runnable {
         private final String command;
